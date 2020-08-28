82fc709bf976b07e60bd17a13adb974599842be7
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-08-28 12:01:55 -0400

    image upload working now

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :username, :password, :pro_pic
 
-  def featured_image
+  def pro_pic
     if object.pro_pic.attached?
       {
         url: rails_blob_url(object.pro_pic)
