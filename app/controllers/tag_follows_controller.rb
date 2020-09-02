96b4e64d68bef14e35d5050db4db672927a45c4e
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-01 22:27:25 -0400

    sad

---
+++ app/controllers/tag_follows_controller.rb
@@ -0,0 +1,19 @@
+class TagFollowsController < ApplicationController
+
+    def index
+        @tag_follows = TagFollow.all
+        render json: @tag_follows
+    end
+
+    def create
+        @tag_follow = UserFish.create(tag_follow_params)
+        render json: @tag_follow
+    end
+
+    private
+
+    def tag_follow_params
+        params.permit(:user_id, :tag_id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :bugs
   resources :fish
   resources :users
+  resources :tag_follows
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
