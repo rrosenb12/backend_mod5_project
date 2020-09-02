cecba554f1031e3145feddd2898ae63e1355e745
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-01 22:05:18 -0400

    tomorrow

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :bugs
   resources :fish
   resources :users
+  resources :tag_follows
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
